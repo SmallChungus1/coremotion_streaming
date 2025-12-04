6a1749458e4a0d625ada6afb15481ab348e807e7
SmallChungus1 <hanson.l@wustl.edu>
2025-12-03 20:42:04 -0600

    added airflow dag

--- dags/spark_email_dag.py
+++ dags/spark_email_dag.py
@@ -2,14 +2,29 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from airflow.operators.python import PythonOperator
-from analytics.charts import SnowflakeCharts, email_to_user
 from dotenv import load_dotenv
 import os
 
-def run_analytics(ds, **_):
-    load_dotenv(os.getenv("ANALYTICS_ENV")) #path to the .env file in analytics folder
+import sys
+#add the analytics folder path for airflow
+sys.path.append("/Users/hansonli/Desktop/coremotion_streaming")
+from analytics.charts import SnowflakeCharts, email_to_user
+from datetime import date
+import pendulum
+
+
+cst_tz = pendulum.timezone("America/Chicago")
+
+PROJECT_ROOT = "/Users/hansonli/Desktop/coremotion_streaming"
+
+SPARK_APP = f"{PROJECT_ROOT}/spark_pipeline/kafka_to_snowflake.py"
+ANALYTICS_ENV = f"{PROJECT_ROOT}/analytics/.env"
+
+def run_analytics(cur_date, analytics_env_path):
+    '''helper function to call the analytics functions + user email send'''
+    load_dotenv(analytics_env_path) #path to the .env file in analytics folder
     charts = SnowflakeCharts()
-    recipients = charts.generate_daily_weekly_charts(cur_date=ds)
+    recipients = charts.generate_daily_weekly_charts(cur_date=cur_date)
     email_to_user(user_email_list=recipients)
 
 default_args = {"owner": "coremotion", "retries": 1, "retry_delay": timedelta(seconds=20)}
@@ -17,24 +32,29 @@ default_args = {"owner": "coremotion", "retries": 1, "retry_delay": timedelta(se
 with DAG(
     "spark_ingest_and_email",
     default_args=default_args,
-    schedule="0 8 * * *",
-    start_date=datetime(2024, 1, 1),
+    schedule=None, #run at noon and 11pm "0 9,21 * * *"
+    start_date=datetime(2025, 11, 1, tzinfo=cst_tz), #set tz to cst
     catchup=False,
 ) as dag:
     spark_ingest = SparkSubmitOperator(
-        task_id="kafka_to_snowflake",
-        application="/opt/airflow/dags/spark_pipeline/kafka_to_snowflake.py",
+        task_id="kafka_to_snowflake_spark",
+        application=SPARK_APP,
         name="kafka2snowflake",
         packages="org.apache.spark:spark-sql-kafka-0-10_2.13:3.5.1,net.snowflake:spark-snowflake_2.13:3.0.0",
-        env_vars={"ANALYTICS_ENV": "/opt/airflow/dags/analytics/.env"},
-        conn_id="spark_default",
+        env_vars={
+            "ANALYTICS_ENV": ANALYTICS_ENV
+        },
+        # IMPORTANT: let it use the local spark-submit on your PATH
+        conn_id=None,
     )
 
     send_email = PythonOperator(
-        task_id="generate_charts_and_email",
+        task_id="gen_charts_and_send_emails",
         python_callable=run_analytics,
-        provide_context=True,
-        env={"ANALYTICS_ENV": "/opt/airflow/dags/analytics/.env"},
+        op_kwargs={
+            "cur_date": date.today().strftime("%Y-%m-%d"),
+            "analytics_env_path": ANALYTICS_ENV
+        },
     )
 
     spark_ingest >> send_email
